eb0dacafe7e8e8dcfb34aee0e738e44cf75c9f3b
George Shaiju <65544308+GeorgeShaijuu@users.noreply.github.com>
2024-03-24 13:50:17 +0530

    .

--- WindowsApp1/Form2.Designer.vb
+++ WindowsApp1/Form2.Designer.vb
@@ -23,13 +23,11 @@ Partial Class Form2
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.TextBox6 = New System.Windows.Forms.TextBox()
@@ -50,21 +48,11 @@ Partial Class Form2
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Course List"
         '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(64, 79)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(157, 19)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Course ID                    :"
-        '
         'Label3
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft YaHei", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(65, 122)
+        Me.Label3.Location = New System.Drawing.Point(112, 98)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(155, 19)
         Me.Label3.TabIndex = 2
@@ -74,7 +62,7 @@ Partial Class Form2
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft YaHei", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(65, 157)
+        Me.Label4.Location = New System.Drawing.Point(112, 133)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(154, 19)
         Me.Label4.TabIndex = 3
@@ -84,7 +72,7 @@ Partial Class Form2
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Microsoft YaHei", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(65, 194)
+        Me.Label5.Location = New System.Drawing.Point(112, 170)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(160, 19)
         Me.Label5.TabIndex = 4
@@ -94,7 +82,7 @@ Partial Class Form2
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft YaHei", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(65, 231)
+        Me.Label6.Location = New System.Drawing.Point(112, 207)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(157, 19)
         Me.Label6.TabIndex = 5
@@ -104,36 +92,29 @@ Partial Class Form2
         '
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("Microsoft YaHei", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label7.Location = New System.Drawing.Point(65, 270)
+        Me.Label7.Location = New System.Drawing.Point(112, 246)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(154, 19)
         Me.Label7.TabIndex = 6
         Me.Label7.Text = "Couse Strength          :"
         '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(278, 78)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 7
-        '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(278, 150)
+        Me.TextBox3.Location = New System.Drawing.Point(325, 126)
         Me.TextBox3.Name = "TextBox3"
         Me.TextBox3.Size = New System.Drawing.Size(100, 20)
         Me.TextBox3.TabIndex = 9
         '
         'TextBox4
         '
-        Me.TextBox4.Location = New System.Drawing.Point(278, 187)
+        Me.TextBox4.Location = New System.Drawing.Point(325, 163)
         Me.TextBox4.Name = "TextBox4"
         Me.TextBox4.Size = New System.Drawing.Size(100, 20)
         Me.TextBox4.TabIndex = 10
         '
         'TextBox6
         '
-        Me.TextBox6.Location = New System.Drawing.Point(278, 270)
+        Me.TextBox6.Location = New System.Drawing.Point(325, 246)
         Me.TextBox6.Name = "TextBox6"
         Me.TextBox6.Size = New System.Drawing.Size(100, 20)
         Me.TextBox6.TabIndex = 12
@@ -172,7 +153,7 @@ Partial Class Form2
         '
         Me.ComboBox1.FormattingEnabled = True
         Me.ComboBox1.Items.AddRange(New Object() {"Phthon technologies", "Java technologies", "Databases", "Computer programming"})
-        Me.ComboBox1.Location = New System.Drawing.Point(278, 120)
+        Me.ComboBox1.Location = New System.Drawing.Point(325, 96)
         Me.ComboBox1.Name = "ComboBox1"
         Me.ComboBox1.Size = New System.Drawing.Size(100, 21)
         Me.ComboBox1.TabIndex = 17
@@ -181,7 +162,7 @@ Partial Class Form2
         '
         Me.ComboBox2.FormattingEnabled = True
         Me.ComboBox2.Items.AddRange(New Object() {"3 MONTHS", "4 MONTHS", "6 MONTHS"})
-        Me.ComboBox2.Location = New System.Drawing.Point(278, 231)
+        Me.ComboBox2.Location = New System.Drawing.Point(325, 207)
         Me.ComboBox2.Name = "ComboBox2"
         Me.ComboBox2.Size = New System.Drawing.Size(100, 21)
         Me.ComboBox2.TabIndex = 18
@@ -199,13 +180,11 @@ Partial Class Form2
         Me.Controls.Add(Me.TextBox6)
         Me.Controls.Add(Me.TextBox4)
         Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form2"
         Me.Text = "COURSE LIST"
@@ -215,13 +194,11 @@ Partial Class Form2
     End Sub
 
     Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
-    Friend WithEvents TextBox1 As TextBox
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents TextBox4 As TextBox
     Friend WithEvents TextBox6 As TextBox
--- WindowsApp1/Form2.vb
+++ WindowsApp1/Form2.vb
@@ -1,23 +1,21 @@
-﻿Imports System.Runtime.Remoting.Services
+﻿Imports System.Data.SqlClient
+Imports System.Runtime.Remoting.Services
 Imports System.Text.RegularExpressions
 Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports MySql.Data.MySqlClient
+
 
 Public Class Form2
+
+    Dim connectionString As String = "server=localhost;user=root;password=admin;database=tms;"
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.Hide()
         home.Show()
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim courseId As String = TextBox1.Text.Trim()
-        If String.IsNullOrEmpty(courseId) Then
-            MessageBox.Show("Please enter a course ID.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
 
-
-        ElseIf Not System.Text.RegularExpressions.Regex.IsMatch(courseId, "^C[1-9]\d{0,4}$") Then
-            MessageBox.Show("Invalid course ID.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-
-        End If
 
 
 
@@ -57,9 +55,32 @@ Public Class Form2
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Assuming Form3 is correctly set up and intended to be shown next
         Me.Hide()
         Form3.Show()
 
+        ' Database insertion starts here
+        Dim connectionString As String = "server=localhost;user=root;password=admin;database=tms;"
+        Try
+            Using connection As New MySqlConnection(connectionString)
+                Dim query As String = "INSERT INTO courses (cname, ctutionfees, cmaterialfees, Cduration, Cstrength) VALUES (@cname, @ctutionfees, @cmaterialfees, @Cduration, @Cstrength)"
+
+                Using command As New MySqlCommand(query, connection)
+                    ' Add parameters with values from your form controls
+                    command.Parameters.AddWithValue("@cname", ComboBox1.Text)
+                    command.Parameters.AddWithValue("@ctutionfees", TextBox3.Text)
+                    command.Parameters.AddWithValue("@cmaterialfees", TextBox4.Text)
+                    command.Parameters.AddWithValue("@Cduration", ComboBox2.Text)
+                    command.Parameters.AddWithValue("@Cstrength", TextBox6.Text)
+
+                    connection.Open()
+                    command.ExecuteNonQuery()
+                    MessageBox.Show("Course data saved successfully.")
+                End Using
+            End Using
+        Catch ex As Exception
+            MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
     End Sub
 
 
